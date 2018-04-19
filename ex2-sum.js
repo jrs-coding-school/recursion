bf42ae1ebdceb7eedfce4f85e3cc70b4b56a8f57
tripott <tripott@gmail.com>
2018-04-19 15:09:40 -0400

    ES6 Array Destructuring

--- ex2-sum.js
+++ ex2-sum.js
@@ -1,18 +1,22 @@
-function sum(acc, arr) {
-  console.log('acc', acc, 'arr', arr)
-  if (arr.length === 0) {
-    return acc
-  } else if (arr.length === 1) {
-    return acc + arr[0]
-  } else {
-    /*
-    The slice() method returns a shallow copy
-     of a portion of an array into a new array object
-     selected from begin to end (end not included).
-    The original array will not be modified.
-    */
-    return sum(acc + arr[0] + arr[1], arr.slice(2))
-  }
-}
+// ES6 With Arrow Function and Array destructing assignment operators
+const sum = (acc, [head, ...rest]) => (!head ? acc : sum(acc + head, rest))
 
 console.log('sum(0, [9000,900,90,9,1])  // =>', sum(0, [9000, 900, 90, 9, 1]))
+
+//  Awld Skool
+// function sum(acc, arr) {
+//   console.log('acc', acc, 'arr', arr)
+//   if (arr.length === 0) {
+//     return acc
+//   } else if (arr.length === 1) {
+//     return acc + arr[0]
+//   } else {
+//     /*
+//     The slice() method returns a shallow copy
+//      of a portion of an array into a new array object
+//      selected from begin to end (end not included).
+//     The original array will not be modified.
+//     */
+//     return sum(acc + arr[0] + arr[1], arr.slice(2))
+//   }
+// }
