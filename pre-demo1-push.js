91d833e282f54e4ca74127689b471ae3392204d9
tripott <tripott@gmail.com>
2018-04-23 15:33:08 -0400

    pre demo and mergesort

---
+++ pre-demo1-push.js
@@ -0,0 +1,13 @@
+const myArr = [1, 2, 3, 4, 5]
+console.log('before push       ', myArr)
+myArr.push(6)
+console.log('after pushing 6   ', myArr)
+
+myArr.pop()
+console.log('after pop         ', myArr)
+
+myArr.unshift(6)
+console.log('after unshifting 6', myArr)
+
+myArr.shift()
+console.log('after shift       ', myArr)
